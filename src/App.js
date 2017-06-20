45fe236550b68fc0015e7e515623eab5c9d4cb27
Elizabet R. Pavlova <elizabet.pavlova@MUSALA.COM>
2017-06-20 14:49:15 +0300

    newCommit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 var React = require('react');
 require('./App.css');
 var Rooms = require('./Rooms.js');
-
+//TEst comment
 class App extends React.Component {
   render() {
     return (
       <div className="loadingDiv">
-        <div className="App"> 
+        <div className="App">
           <Rooms />
         </div>
       </div>
